ddfd070148bdd15d33c6fb45c22ee996f655212e
Karthik Raja <kar304@pitt.edu>
2024-10-02 19:38:07 -0400

    Power Function is correct

--- TrainController/tempCodeRunnerFile.py
+++ TrainController/tempCodeRunnerFile.py
@@ -1,3 +1,1 @@
-ency_brake)
-        # emergency_brake_button.pressed.connect(self.train_controller.start_braking)
-        # emergency_brake_button.released.connect(self.train_controller.stop_braking)
+t
